ce4156e3eda8c9d01a15e9a7db71c744ebf9f5f3
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-06 21:59:05 -0300

    Arquivo test Seeding

---
+++ src/main/java/com/claytoneduard/services/config/TestConfig.java
@@ -0,0 +1,36 @@
+package com.claytoneduard.services.config;
+
+import com.claytoneduard.services.entities.User;
+import com.claytoneduard.services.repositories.UserRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+    User u1 = new User(
+      null,
+      "Maria Brown",
+      "maria@gmail.com",
+      "988888888",
+      "123456"
+    );
+    User u2 = new User(
+      null,
+      "Alex Green",
+      "alex@gmail.com",
+      "977777777",
+      "123456"
+    );
+    userRepository.saveAll(Arrays.asList(u1, u2));
+  }
+}
