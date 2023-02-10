3029867440786e91bebff24343790b071b98f8dc
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-10 20:18:07 -0300

    Create class order

--- src/main/java/com/claytoneduard/services/entities/Order.java
+++ src/main/java/com/claytoneduard/services/entities/Order.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -20,6 +22,7 @@ public class Order implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
   private Instant moment;
 
   // associancao ao classe cliente
