f6cec11baa3459c00258e75a255b318d80ffff42
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-10 20:17:47 -0300

    Criando teste de Order

--- src/main/java/com/claytoneduard/services/config/TestConfig.java
+++ src/main/java/com/claytoneduard/services/config/TestConfig.java
@@ -1,7 +1,10 @@
 package com.claytoneduard.services.config;
 
+import com.claytoneduard.services.entities.Order;
 import com.claytoneduard.services.entities.User;
+import com.claytoneduard.services.repositories.OrderRepository;
 import com.claytoneduard.services.repositories.UserRepository;
+import java.time.Instant;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +17,9 @@ public class TestConfig implements CommandLineRunner {
 
   @Autowired
   private UserRepository userRepository;
+
+  @Autowired
+  private OrderRepository orderRepository;
 
   @Override
   public void run(String... args) throws Exception {
@@ -31,6 +37,12 @@ public class TestConfig implements CommandLineRunner {
       "977777777",
       "123456"
     );
+    // formato da data ISO 8601
+    Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+    Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+    Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
     userRepository.saveAll(Arrays.asList(u1, u2));
+    orderRepository.saveAll(Arrays.asList(o1, o2, o3));
   }
 }
