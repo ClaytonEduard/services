f8365aba16f16775ab129641999fcd3885cd6a7e
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-11 16:18:42 -0300

    Association OrderStatus

--- src/main/java/com/claytoneduard/services/config/TestConfig.java
+++ src/main/java/com/claytoneduard/services/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.claytoneduard.services.config;
 
 import com.claytoneduard.services.entities.Order;
 import com.claytoneduard.services.entities.User;
+import com.claytoneduard.services.entities.enums.OrderStatus;
 import com.claytoneduard.services.repositories.OrderRepository;
 import com.claytoneduard.services.repositories.UserRepository;
 import java.time.Instant;
@@ -21,6 +22,7 @@ public class TestConfig implements CommandLineRunner {
   @Autowired
   private OrderRepository orderRepository;
 
+
   @Override
   public void run(String... args) throws Exception {
     User u1 = new User(
@@ -38,9 +40,9 @@ public class TestConfig implements CommandLineRunner {
       "123456"
     );
     // formato da data ISO 8601
-    Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-    Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-    Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+    Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+    Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+    Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
