12fe8cdd7b4dab3faecdf940cfd0a1cab886927a
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-11 16:17:49 -0300

    Create Class enum OrderStatus

---
+++ src/main/java/com/claytoneduard/services/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.claytoneduard.services.entities.enums;
+
+public enum OrderStatus {
+  // definir a numeracao para manutencoes posteriores
+
+  WAITING_PAYMENT(1),
+  PAID(2),
+  SHIPPED(3),
+  DELIVERED(4),
+  CANCELED(5);
+
+  private int code;
+
+  // cosntrutor
+  private OrderStatus(int code) {
+    this.code = code;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public static OrderStatus valueOf(int code) {
+    for (OrderStatus value : OrderStatus.values()) {
+      if (value.getCode() == code) {
+        return value;
+      }
+    }
+    throw new IllegalArgumentException("Invalid OrderStatus code");
+  }
+}
