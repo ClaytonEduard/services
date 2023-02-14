9bac3fbc18997b8458a5757c9ab118c6c70d3bf8
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-14 09:17:33 -0300

    Create Teste ManyToMany

--- src/main/java/com/claytoneduard/services/config/TestConfig.java
+++ src/main/java/com/claytoneduard/services/config/TestConfig.java
@@ -109,6 +109,18 @@ public class TestConfig implements CommandLineRunner {
 
     categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+    //adicionando os produtos a categorias
+    p1.getCategories().add(cat2);
+    // SmartTV pertence tanto para eletronicos quanto para Computers
+    p2.getCategories().add(cat1);
+    p2.getCategories().add(cat3);
+    //--------------------------------------------------------------
+    p3.getCategories().add(cat3);
+    p4.getCategories().add(cat3);
+    p5.getCategories().add(cat2);
+    // salvando novamente para 
+    productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
   }
