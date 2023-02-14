f3fbec757ed80bb06f3dedb2b3435ae16f623804
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-14 09:17:12 -0300

    Create associations ManyToMany with Product

--- src/main/java/com/claytoneduard/services/entities/Category.java
+++ src/main/java/com/claytoneduard/services/entities/Category.java
@@ -1,11 +1,12 @@
 package com.claytoneduard.services.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -22,7 +23,15 @@ public class Category implements Serializable {
 
   private String name;
 
-  @Transient
+  /*
+   * Mapeamento de vinculo com a tabela produto ,
+   * sendo so que a Interface de categorias possue o,
+   * nome categories na tabela produto
+   *
+   * @JsonIgnore para quando listar não entrar em looop
+   */
+  @JsonIgnore
+  @ManyToMany(mappedBy = "categories")
   private Set<Product> products = new HashSet<>();
 
   public Set<Product> getProducts() {
