a73e1ee11e8e3637b38a07cd6f21c63c3bd77255
ClaytonEduard <clayton_eduard@hotmail.com>
2023-05-30 15:02:52 -0300

    User update

--- src/main/java/com/claytoneduard/services/servicess/UserService.java
+++ src/main/java/com/claytoneduard/services/servicess/UserService.java
@@ -4,6 +4,7 @@ import com.claytoneduard.services.entities.User;
 import com.claytoneduard.services.repositories.UserRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,8 +29,20 @@ public class UserService {
   }
 
   //deleção do user
-  public void delete(Long id){
+  public void delete(Long id) {
     repository.deleteById(id);
   }
 
+  // atualizar usuario
+  public User update(Long id, User obj) {
+    User entity = repository.getReferenceById(id); // busca o objeto no banco;
+    updateData(entity, obj);
+    return repository.save(entity);
+  }
+
+  private void updateData(User entity, User obj) {
+    entity.setEmail(obj.getEmail());
+    entity.setName(obj.getName());
+    entity.setPhone(obj.getPhone());
+  }
 }
