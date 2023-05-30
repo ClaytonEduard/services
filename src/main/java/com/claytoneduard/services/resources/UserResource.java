4bb15c810c1a4d9760b08ca45e6a02faabf94abc
ClaytonEduard <clayton_eduard@hotmail.com>
2023-05-30 12:51:15 -0300

    User insert

--- src/main/java/com/claytoneduard/services/resources/UserResource.java
+++ src/main/java/com/claytoneduard/services/resources/UserResource.java
@@ -2,13 +2,17 @@ package com.claytoneduard.services.resources;
 
 import com.claytoneduard.services.entities.User;
 import com.claytoneduard.services.servicess.UserService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -17,6 +21,7 @@ public class UserResource {
   @Autowired
   private UserService service;
 
+  //recupar os dados do banco
   @GetMapping
   public ResponseEntity<List<User>> findAll() {
     List<User> list = service.findAll();
@@ -24,10 +29,22 @@ public class UserResource {
     return ResponseEntity.ok().body(list);
   }
 
+  //recupar os dados do banco
   @GetMapping(value = "/{id}")
   public ResponseEntity<User> findById(@PathVariable Long id) {
-      User obj = service.findById(id);
-    return ResponseEntity.ok().body(obj);
+    User obj = service.findById(id);
+    return ResponseEntity.ok().body(obj); // retorna a resposta Ok com o corpo do objeto
   }
 
+  @PostMapping
+  public ResponseEntity<User> insert(@RequestBody User obj) {
+    //TODO: process POST request
+    obj = service.insert(obj);
+    URI uri = ServletUriComponentsBuilder
+      .fromCurrentRequest()
+      .path("/{id}")
+      .buildAndExpand(obj.getId())
+      .toUri();
+    return ResponseEntity.created(uri).body(obj); // created recebe uma URI para retornar o status 201
+  }
 }
